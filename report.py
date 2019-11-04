e2929c8e65e65c35c2a78c2a09c28097b191b57a
Ian Caragol <iancaragol@gmail.com>
2019-11-04 11:44:22 -0700

    Initial commit.

---
+++ report.py
@@ -0,0 +1,62 @@
+import pyodbc
+import datetime as dt
+
+class Report:
+    def __init__(self, loc_name, loc_lat, loc_long, description, severity, closure):
+        self.id = 0 # Int
+        self.loc_name = loc_name # String
+        self.loc_lat = loc_lat # Float
+        self.loc_long = loc_long # Float
+        self.report_description = description # String
+        self.severity = severity # Int
+        self.closure = 0 # Bit
+        self.report_datetime = dt.now().strftime("%m/%d/%Y, %H:%M:%S") # String
+
+    def set_id(self, id):
+        self.id = id
+
+    def insert_string(self):
+        return "{}, {}, {}, {}, {}, {}, {}".format(self.loc_name, str(self.loc_lat), str(self.loc_long), self.description, str(self.severity, str(self.closure), self.report_datetime))
+
+class ReportHandler:
+    def __init__(self):
+        server = 'parkwatch-db-server.database.windows.net'
+        database = 'parkwatch-database'
+        username = 'ranger'
+        password = 'ParkWatch123!'
+        driver = '{ODBC Driver 13 for SQL Server};Server=tcp:parkwatch-db-server.database.windows.net,1433;Database=parkwatch-database;Uid=ranger;Pwd=ParkWatch123!;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30'
+        cnxn = pyodbc.connect(driver)
+
+        self.cnxn = cnxn
+        self.cursor = cnxn.cursor()
+        self.report_dictionary = {}
+
+    def create_report(self, loc_name, loc_lat, loc_long, description, severity, closure):
+        print("Create report")
+
+        new_report = Report(loc_name, loc_lat, loc_long, description, severity, closure) # Create a new report
+
+        self.cursor.execute("insert into reports(loc_name, loc_lat, loc_long, report_description, severity, closure, report_datetime) values ({})".format(new_report.insert_string())) # Insert it into databse
+        self.cnxn.commit()
+        self.cursor("select SCOPE_IDENTITY()")
+        self.cnxn.commit()
+
+        new_id = int(self.cursor.fetchone())
+        new_report.set_id(new_id)
+
+        # Commit report with new id
+        self.cursor.execute("insert into reports(id) values ({})".format(str(new_report.id))) # Insert it into databse
+        self.cnxn.commit()
+
+        self.report_dictionary[new_id] = new_report
+
+
+def main():
+    print("Main")
+    rh = ReportHandler()
+    rh.create_report("test", 1.0, 1.0, "some description", 10, 0)
+
+if __name__ == "__main__":
+    main()
+
+
