120c55563addd35448e55eb85f65dd099b120894
ericsm94 <eric_miramontes@outlook.com>
2020-02-21 14:31:20 -0700

    Sanitized db input for getting list of reports

--- controllers/report.py
+++ controllers/report.py
@@ -144,10 +144,13 @@ class ReportHandler:
         """
         print("Getting reports.")
 
-        selection_string = "Select id, loc_name, loc_lat, loc_long, description, severity, closure, date, park_id, approved_status From Reports Where park_id = {}".format(park_id)
+        selection_string = textwrap.dedent("""
+            Select id, loc_name, loc_lat, loc_long, description, severity, closure, date, park_id, approved_status 
+            From Reports 
+            Where park_id = ?""")
         
         try:
-            return self.get_list_helper(selection_string)
+            return self.get_list_helper(selection_string, park_id)
         except Exception as e:
             print('Encountered database error while retrieving a list of reports.\nRetrying.\n{}'.format(str(e)))
             cnxn = pyodbc.connect(driver)
@@ -155,11 +158,11 @@ class ReportHandler:
             self.cnxn = cnxn
             self.cursor = cnxn.cursor()
 
-            return self.get_list_helper(selection_string)
+            return self.get_list_helper(selection_string, park_id)
         return None
     
-    def get_list_helper(self, query):
-        self.cursor.execute(query)
+    def get_list_helper(self, query, park_id):
+        self.cursor.execute(query, park_id)
         results = self.cursor.fetchall()
         reports = []
         if results:
